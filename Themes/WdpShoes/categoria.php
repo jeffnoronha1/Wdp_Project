f34875591aa83a2a2f28aa2cd4a3ad290d0c56d7
jeffnoronha777 <62403545+jeffnoronha777@users.noreply.github.com>
2021-07-12 11:41:46 -0300

    Estilizado e responsivado página de categoria

---
+++ Themes/WdpShoes/categoria.php
@@ -0,0 +1,56 @@
+<main class="main_content">
+  <div class="container main_category">
+    <section class="main_category_products">
+      <header class="main_category_products_header">
+        <h1>Nome da Categoria:</h1>
+        <form action="">
+          <p>Ordenar por:</p>
+          <select name="" id="">
+            <option value="">Mais vendidos</option>
+            <option value="">Ofertas especiais</option>
+            <option value="">Lançamentos</option>
+            <option value="">Menor preço</option>
+            <option value="">Maior preço</option>
+          </select>
+        </form>
+      </header>
+
+      <div class="main_category_products_content">
+        <?php
+
+        for ($pdt = 1; $pdt < 16; $pdt++) {
+          require "{$configThemePath}/Templates/product.php";
+        }
+        ?>
+      </div>
+
+      <nav class="main_category_products_paginator">
+        <a href="#" class="radius transition active">1</a>
+        <a href="#" class="radius transition">2</a>
+        <a href="#" class="radius transition">3</a>
+        <a href="#" class="radius transition">4</a>
+        <a href="#" class="radius transition">5</a>
+      </nav>
+    </section>
+    <aside class="main_category_sidebar">
+      <h2 class="main_category_sidebar_title">Filtrar resultados:</h2>
+
+      <?php
+
+      for ($m = 1; $m < 4; $m++) {
+      ?>
+        <nav class="main_category_sidebar_widget">
+          <h3 class="main_category_sidebar_widget_title">Menu de filtro:</h3>
+          <a class="transition radius" href="#" title="">Filtro (tamanho/cor/etc)</a>
+          <a class="transition radius" href="#" title="">Filtro (tamanho/cor/etc)</a>
+          <a class="transition radius" href="#" title="">Filtro (tamanho/cor/etc)</a>
+          <a class="transition radius" href="#" title="">Filtro (tamanho/cor/etc)</a>
+          <a class="transition radius" href="#" title="">Filtro (tamanho/cor/etc)</a>
+          <a class="transition radius" href="#" title="">Filtro (tamanho/cor/etc)</a>
+        </nav>
+      <?php
+      }
+      ?>
+    </aside>
+  </div>
+</main>
